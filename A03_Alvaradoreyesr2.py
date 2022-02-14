814068be15901a87bc53d5623cff626b842ef7d9
RodolfoAlvarado1 <89101642+RodolfoAlvarado1@users.noreply.github.com>
2022-02-14 01:04:59 -0500

    completed

--- A03_Alvaradoreyesr2.py
+++ A03_Alvaradoreyesr2.py
@@ -2,11 +2,17 @@
 # Username: Alvaradoreyesr2
 import turtle
 from threading import Thread
+import time
 
 wn = turtle.Screen()
 house = turtle.Turtle()
 door = turtle.Turtle()
-wn.bgcolor("light blue")
+nob = turtle.Turtle()
+newin = turtle.Turtle()
+wn.bgcolor("#3776ab")
+wn.bgpic("background.gif")
+
+
 
 
 def border(width,height):
@@ -29,43 +35,80 @@ def front():
     door.left(180)
     door.forward(80)
     door.left(90)
-    door.forward(100)
+    door.forward(85)
     door.left(180)
     door.pendown()
+    door.fillcolor("brown")
+    door.begin_fill()
     for i in range(2):
         door.forward(100)
         door.right(90)
         door.forward(30)
         door.right(90)
+    door.end_fill()
+    frontnob()
+
+def frontnob():
+    nob.penup()
+    nob.color("grey")
+    nob.left(180)
+    nob.forward(70)
+    nob.left(90)
+    nob.forward(40)
+
+def createwindow():
+    newin.penup()
+    newin.left(180)
+    newin.forward(150)
+    newin.right(90)
+    newin.forward(50)
+    newin.right(90)
+    newin.pendown()
+    for i in range(2):
+        newin.left(90)
+        newin.forward(50)
+        newin.left(90)
+        newin.forward(50)
+    for i in range(2):
+        newin.right(-135)
+        newin.forward(71)
+        newin.left(135)
+        newin.forward(50)
 
 
 
-long = True
-
-while long:
-    width = int(input("What width do you want the house to have"))
-    if width < 250:
-        print("this is too short")
-    elif width >700:
-        print("house is too long")
-    else:
-        long = False
-
-tall = True
-while tall:
-    height = int(input("What height do you want the house to have"))
-    if height <250:
-        print("house is too short")
-    elif height >300:
-        print("house is too tall")
-    else:
-        tall = False
 
 
 
 
-w = border(width,height)
-front()
+def main():
+    long = True
+
+    while long:
+        width = int(input("What width do you want the house to have"))
+        if width < 250:
+            print("this is too short")
+        elif width > 700:
+            print("house is too long")
+        else:
+            long = False
+
+    tall = True
+    while tall:
+        height = int(input("What height do you want the house to have"))
+        if height < 250:
+            print("house is too short")
+        elif height > 300:
+            print("house is too tall")
+        else:
+            tall = False
+    Thread(target=border(width, height)).start()
+    Thread(target= front).start()
+    Thread(target = createwindow).start()
+
+if __name__ == '__main__':
+    main()
+
 
 
 wn.exitonclick()
