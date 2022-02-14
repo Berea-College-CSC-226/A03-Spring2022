0805aa1f69a14f653a5e80bc7edb3ea07a76df4c
Nattyjr <abeben@berea.edu>
2022-02-14 00:19:50 -0500

    Illusion cirlce and a church design

--- a03_abeben.py
+++ a03_abeben.py
@@ -7,7 +7,7 @@
 # Google Doc Link: https://docs.google.com/document/d/16lCjQwgEN9DpMXIOVi1iF42yxxbKww_ov-1BztKZfhU/edit?usp=sharing
 #
 #################################################################################
-# Acknowledgements:
+# Acknowledgements: Stackflow- refer the loop to create the triangle
 #
 #
 #################################################################################
@@ -19,7 +19,7 @@ from turtle import *
 def square (t, length, pensize, color,):
     #Setting the position, pensize, and color
     t.penup()
-    t.setpos(30, -10)
+    t.setpos(210, 110)
     t.pensize(pensize)
     t.color(color)
     t.pendown()
@@ -27,6 +27,12 @@ def square (t, length, pensize, color,):
     for i in range(4):
         t.forward(length)
         t.right(90)
+
+        t.penup()
+        t.setpos(210, 110)
+        t.pensize(pensize)
+        t.color(color)
+        t.pendown()
 
     ## To create the triangele
 
@@ -43,26 +49,25 @@ def triangle(t, length, color):
     t.left(135)
     t.end_fill()
 
+    for i in range(4):   #### loop fpr triangle
+        t.forward(length)
+        t.right(90)
+        t.color("green")
+        t.pensize(5)
+    #### Creates the illusion circle
+def circle(t,length, color):
+    circle = turtle.Turtle()
+    circle.penup()
+    circle.pencolor("blue")
+    circle.setpos(100, 110)
+    circle.left(43)
+    circle.shapesize(40)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Creates the Circle
+    for i in range(100):
+        circle.stamp()
+        circle.forward(13.6)
+        circle.right(3.6)
 
 def main():
 
@@ -70,11 +75,10 @@ def main():
     wn.bgcolor('black')
     square_turtle = turtle.Turtle()
     triangle_turtle = turtle.Turtle()
-    triangle(triangle_turtle, 250,"red")
-
-    square(square_turtle, 350, 25, "blue")
-
-
+    triangle(triangle_turtle, 250,"#e51212")
+    square(square_turtle, 20, 25, "#dee9e1")
+    circle_turtle = turtle.Turtle()
+    circle(circle_turtle, 2, "red" )
     wn.bgcolor('black')
 
     wn.exitonclick()
